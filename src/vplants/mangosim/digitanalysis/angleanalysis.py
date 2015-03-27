365450a9a2b1631e8d166ba2b061989253407313
Frederic Boudon <frederic.boudon@cirad.fr>
2015-03-27 07:35:34 +0000

    fix bug with mtg path
    git-svn-id: https://scm.gforge.inria.fr/authscm/boudon/svn/vplants/vplants/branches/mangosim@17928 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/vplants/mangosim/digitanalysis/angleanalysis.py
+++ src/vplants/mangosim/digitanalysis/angleanalysis.py
@@ -1,3 +1,10 @@
+from os.path import join, exists
+from openalea.deploy.shared_data import shared_data
+import vplants.mangosim
+share_dir = shared_data(vplants.mangosim, share_path = join("share",'digitized_mango_mtg'))
+
+def get_mtg_filename(name):
+	return join(share_dir,name)
 
 def get_ucs(m):
     return [e for e in m.vertices(scale=2) if m.class_name(e) == 'U']
@@ -46,7 +53,8 @@ def plot_histo(angles):
 
 if __name__ == '__main__':
     from openalea.mtg import *
-    #m = MTG("a19.mtg")
+
+    m = MTG(get_mtg_filename("a19.mtg"))
     #m = MTG("jf31.mtg")
     ucs = get_ucs(m)
     #print ucfirstpos(ucs[0],m)
