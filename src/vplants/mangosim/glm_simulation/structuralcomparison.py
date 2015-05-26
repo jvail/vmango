3014506d471fc600a6ab5b1719df3296e3cafd40
Frederic Boudon <frederic.boudon@cirad.fr>
2015-05-26 08:20:18 +0000

    structuralcomparison improve
    git-svn-id: https://scm.gforge.inria.fr/authscm/boudon/svn/vplants/vplants/branches/mangosim@18337 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/vplants/mangosim/glm_simulation/structuralcomparison.py
+++ src/vplants/mangosim/glm_simulation/structuralcomparison.py
@@ -17,15 +17,19 @@ def get_burst_date(u, values):
     return bdate
 
 class MtgNodeCost(NodeCost):
-    def __init__(self, refburstdate, simburstdate): 
+    def __init__(self, refburstdate, simburstdate, refmtg, simmtg): 
         NodeCost.__init__(self)
         self.refburstdate = refburstdate
         self.simburstdate = simburstdate
+        self.refmtg = refmtg
+        self.simmtg = simmtg
 
     def getDeletionCost(self,a) : return 1
     def getInsertionCost(self,b) : return 1
 
     def getChangingCost(self,a,b) : 
+        #if self.refmtg.edge_type(a) != self.simmtg.edge_type(b): return 0.5
+        #return 0
         val = abs(month_difference(get_burst_date(a,self.refburstdate), get_burst_date(b,self.simburstdate)))/24.
         if not (0 <= val <= 1):
             raise ValueError(val, get_burst_date(a,self.refburstdate), get_burst_date(b,self.simburstdate), first_date)
@@ -82,7 +86,7 @@ def process(mtgfname):
     global allres
     print 'compare', mtgfname
     res = compare(refmtg, load_obj(mtgfname))
-    allres[mtgfname] = res
+    allres[mtgfname.replace(share_dir,'')] = res
 
 def structural_comparison(wdmv = 0, glmestimation = eNullGlm, nb = None , nbproc = 1):
     global allres
@@ -100,7 +104,7 @@ def structural_comparison(wdmv = 0, glmestimation = eNullGlm, nb = None , nbproc
     if os.path.exists(distrib_file):
         print 'Retreive structural comparision from',repr(str(distrib_file))
         allres = load_obj(structural_comparison_cache,inputdir)
-        mtgfnames = [fname for fname in mtgfnames if not fname in allres]
+        mtgfnames = [fname for fname in mtgfnames if not fname.replace(share_dir,'') in allres]
     else : allres = {}
     if len(mtgfnames) > 0:
         manager = Manager()
@@ -113,6 +117,41 @@ def structural_comparison(wdmv = 0, glmestimation = eNullGlm, nb = None , nbproc
         dump_obj(allres,structural_comparison_cache,inputdir)
     return allres
 
+def comparison_map(wdmv, glmestimation = eNullGlm, nb = 0 ):
+    mm.setMtgStyle(eSimulatedMtg)
+    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : glmestimation}
+    inputdir = get_glm_mtg_repository( params = params)
+    print 'Retrieve MTGs'
+    global refmtg
+    refmtg = get_reference_mtg()
+    mtgfname = retrieve_mtg_filenames(inputdir,nb)[nb]
+    simmtg = load_obj(mtgfname)
+    BurstDatePropertyName = 'burst_date'
+    cost = MtgNodeCost(refmtg.property(BurstDatePropertyName),simmtg.property(BurstDatePropertyName),refmtg, simmtg)
+    root1, root2 = get_trees(refmtg, simmtg)['B12']
+    m = MtgMatching(refmtg, simmtg, scale1=refmtg.max_scale(), scale2=simmtg.max_scale(), root1=root1, root2=root2, cost=cost)
+    res = m.match()/(float(len(m.idmap1))*2.  )
+    print res
+    return m
+
+
+def gcomparison(wdmv = 0):
+    from numpy import mean, std
+    mm.setMtgStyle(eSimulatedMtg)
+    nbrefnode = 1659
+    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : eNullGlm}
+    inputdir1 = get_glm_mtg_repository( params = params)
+    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : eSelectedGlm}
+    inputdir2 = get_glm_mtg_repository( params = params)
+    allres1 = load_obj(structural_comparison_cache,inputdir1)
+    allres2 = load_obj(structural_comparison_cache,inputdir2)
+    print 'NullGlm:',mean(allres1.values())/(2*nbrefnode), ', SelectedGlm:',mean(allres2.values())/(2*nbrefnode)
+    print 'NullGlm:',std(allres1.values())/(2*nbrefnode), ', SelectedGlm:',std(allres2.values())/(2*nbrefnode)
+    return allres1, allres2
+
 if __name__ == '__main__':
-    structural_comparison(0,eSelectedGlm,100,None)
-    structural_comparison(0,eNullGlm,100,None)
+    #structural_comparison(0,eSelectedGlm,100,None)
+    #structural_comparison(0,eNullGlm,100,None)
+    #allres1, allres2 = gcomparison()
+
+    pass
