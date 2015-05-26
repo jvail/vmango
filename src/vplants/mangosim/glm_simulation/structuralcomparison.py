4f3de68f59c7ec64e11f2a392882a147fcef7ab6
Frederic Boudon <frederic.boudon@cirad.fr>
2015-05-26 13:41:16 +0000

    structuralcomparison tree separation
    git-svn-id: https://scm.gforge.inria.fr/authscm/boudon/svn/vplants/vplants/branches/mangosim@18338 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/vplants/mangosim/glm_simulation/structuralcomparison.py
+++ src/vplants/mangosim/glm_simulation/structuralcomparison.py
@@ -46,15 +46,16 @@ def get_trees(refmtg, simmtg):
 
 def compare(refmtg, simmtg):
     BurstDatePropertyName = 'burst_date'
-    cost = MtgNodeCost(refmtg.property(BurstDatePropertyName),simmtg.property(BurstDatePropertyName))
-    allres = []
-    for tname, roots in get_trees(refmtg, simmtg).items():
+    cost = MtgNodeCost(refmtg.property(BurstDatePropertyName),simmtg.property(BurstDatePropertyName),refmtg,simmtg)
+    allres = dict()
+    trees = get_trees(refmtg, simmtg).items()
+    for tname, roots in trees:
         root1, root2 = roots
         m = MtgMatching(refmtg, simmtg, scale1=refmtg.max_scale(), scale2=simmtg.max_scale(), root1=root1, root2=root2, cost=cost)
         res = m.match()
-        print res
-        allres.append(res)
-    return sum(allres)
+        print res/(float(len(m.idmap1))*2.)
+        allres[tname] = res/(float(len(m.idmap1))*2.)
+    return allres
 
 
 def retrieve_mtg_filenames(inputdir, nb = None):
@@ -81,46 +82,63 @@ structural_comparison_cache = 'structural_comparison.pkl'
 
 allres = None
 refmtg = None
+lock = None
+cachefile = None
+
+def get_input_dir(glmestimation = eNullGlm, wdmv = 0):
+    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : glmestimation}
+    inputdir = get_glm_mtg_repository( params = params)
+    return inputdir
+
 
 def process(mtgfname):
     global allres
     print 'compare', mtgfname
     res = compare(refmtg, load_obj(mtgfname))
-    allres[mtgfname.replace(share_dir,'')] = res
+    allres[mtgfname.replace(share_dir+'/','')] = res
+    structural_comparison_cache,inputdir = cachefile
+    allresult = dict(allres)
+    lock.acquire(True)
+    dump_obj(allresult,structural_comparison_cache,inputdir)
+    lock.release()
 
-def structural_comparison(wdmv = 0, glmestimation = eNullGlm, nb = None , nbproc = 1):
-    global allres
+def structural_comparison(glmestimation = eNullGlm, wdmv = 0, nb = None , nbproc = 1):
+    global allres, cachefile, lock
     from multiprocessing import Pool, Manager, cpu_count
     if nbproc is None : nbproc = cpu_count()
     mm.setMtgStyle(eSimulatedMtg)
-    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : glmestimation}
-    inputdir = get_glm_mtg_repository( params = params)
+    inputdir = get_input_dir( glmestimation, wdmv)
     distrib_file = join(inputdir, structural_comparison_cache)
     global refmtg
     refmtg = get_reference_mtg()
     print 'Retrieve MTGs'
     mtgfnames = retrieve_mtg_filenames(inputdir,nb)
 
+    import sys
+    if '--resetcache' in sys.argv:
+        os.remove(distrib_file)
     if os.path.exists(distrib_file):
         print 'Retreive structural comparision from',repr(str(distrib_file))
         allres = load_obj(structural_comparison_cache,inputdir)
-        mtgfnames = [fname for fname in mtgfnames if not fname.replace(share_dir,'') in allres]
+        allres = dict([(k.replace(share_dir+'/',''),v) for k,v in allres.items()])
+        mtgfnames = [fname for fname in mtgfnames if not fname.replace(share_dir+'/','') in allres]
     else : allres = {}
     if len(mtgfnames) > 0:
+        nbprocesses = min(nbproc, len(mtgfnames), cpu_count())        
         manager = Manager()
+        lock = manager.Lock()
         allres = manager.dict(allres)
-        nbprocesses = min(nbproc, len(mtgfnames), cpu_count())
+        cachefile = (structural_comparison_cache,inputdir)
         print 'use',nbprocesses,'processes'
         p = Pool(processes = nbprocesses)
         p.map(process, mtgfnames)
-        allres = dict(allres)
-        dump_obj(allres,structural_comparison_cache,inputdir)
+        allresult = dict(allres)
+        dump_obj(allresult,structural_comparison_cache,inputdir)
     return allres
 
-def comparison_map(wdmv, glmestimation = eNullGlm, nb = 0 ):
+def comparison_map(glmestimation = eNullGlm, nb = 0 ,  wdmv = 0):
     mm.setMtgStyle(eSimulatedMtg)
-    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : glmestimation}
-    inputdir = get_glm_mtg_repository( params = params)
+    inputdir = get_input_dir( glmestimation, wdmv)
     print 'Retrieve MTGs'
     global refmtg
     refmtg = get_reference_mtg()
@@ -138,20 +156,31 @@ def comparison_map(wdmv, glmestimation = eNullGlm, nb = 0 ):
 def gcomparison(wdmv = 0):
     from numpy import mean, std
     mm.setMtgStyle(eSimulatedMtg)
-    nbrefnode = 1659
-    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : eNullGlm}
-    inputdir1 = get_glm_mtg_repository( params = params)
-    params = {'WITHINDELAYMETHOD' : WITHINDELAYMETHODVALUES[wdmv],  'ESTIMATIONTYPE' : eSelectedGlm}
-    inputdir2 = get_glm_mtg_repository( params = params)
+    inputdir1 = get_input_dir( eNullGlm, wdmv) 
+    inputdir2 = get_input_dir( eSelectedGlm, wdmv)  
     allres1 = load_obj(structural_comparison_cache,inputdir1)
     allres2 = load_obj(structural_comparison_cache,inputdir2)
-    print 'NullGlm:',mean(allres1.values())/(2*nbrefnode), ', SelectedGlm:',mean(allres2.values())/(2*nbrefnode)
-    print 'NullGlm:',std(allres1.values())/(2*nbrefnode), ', SelectedGlm:',std(allres2.values())/(2*nbrefnode)
+    allres1 = dict([(k,v) for k,v in allres1.items() if type(v) == dict])
+    allres2 = dict([(k,v) for k,v in allres2.items() if type(v) == dict])
+    for tree in allres1.values()[0].keys():
+        print 'Tree', repr(tree)
+        print 'NullGlm:',mean([v[tree] for v in allres1.values()]), ', SelectedGlm:',mean([v[tree] for v in allres2.values()])
+        print 'NullGlm:',std([v[tree] for v in allres1.values()]), ', SelectedGlm:',std([v[tree] for v in allres2.values()])
     return allres1, allres2
 
 if __name__ == '__main__':
-    #structural_comparison(0,eSelectedGlm,100,None)
-    #structural_comparison(0,eNullGlm,100,None)
+    import sys
+    if '--run' in sys.argv:
+        idx = sys.argv.index('--run')
+        if len(sys.argv) > idx+1:
+            nbtrees = int(sys.argv[idx+1])
+        else : nbtrees = None
+        if len(sys.argv) > idx+2:
+            glm = eval(sys.argv[idx+2])
+        else : glm = eSelectedGlm
+
+        structural_comparison(glm,0,nbtrees,None)
+    #structural_comparison(eNullGlm,0,100,None)
     #allres1, allres2 = gcomparison()
 
     pass
